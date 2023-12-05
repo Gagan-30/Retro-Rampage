3b0ba8dcdb37e91112d26754cacbc812c925f72d
MalwareDetectedBlog <MalwareDetected0@gmail.com>
2023-12-05 12:28:21 +0000

    Added Main Menu Game Title

--- src/main/java/com/base/game/retrorampage/Game.java
+++ src/main/java/com/base/game/retrorampage/Game.java
@@ -1,8 +1,6 @@
 package com.base.game.retrorampage;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
