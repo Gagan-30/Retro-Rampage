0ed3b131a189adc7aa92e979f9324610b4d82723
MalwareDetectedBlog <MalwareDetected0@gmail.com>
2023-12-08 11:04:20 +0000

    Created DifficultySelection class to create new scene.

--- src/main/java/com/base/game/retrorampage/MainMenuController.java
+++ src/main/java/com/base/game/retrorampage/MainMenuController.java
@@ -1,15 +1,9 @@
 package com.base.game.retrorampage;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class MainMenuController {
 
