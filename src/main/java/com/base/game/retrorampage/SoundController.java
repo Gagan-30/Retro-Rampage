7c4bf712347e3a0afe92a30566764a6cebd26c66
MalwareDetectedBlog <MalwareDetected0@gmail.com>
2023-12-16 19:05:06 +0000

    TODO: REGEX and Change text when input = 0

--- src/main/java/com/base/game/retrorampage/SoundController.java
+++ src/main/java/com/base/game/retrorampage/SoundController.java
@@ -1,11 +1,8 @@
 package com.base.game.retrorampage;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
