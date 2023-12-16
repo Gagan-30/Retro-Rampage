b30f3e7402b9a495ef0137307409fb11d765dad8
MalwareDetectedBlog <MalwareDetected0@gmail.com>
2023-12-16 17:07:21 +0000

    Completed Sound settings

--- src/main/java/com/base/game/retrorampage/SoundController.java
+++ src/main/java/com/base/game/retrorampage/SoundController.java
@@ -1,13 +1,32 @@
 package com.base.game.retrorampage;
 
-import javafx.event.ActionEvent;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class SoundController {
     private Scene previousScene;
-    private Stage stage; // Add a stage variable
+    private Stage stage;
+    private int previousVolume = 50;
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    private TextField volumeTextField;
+
+    @FXML
+    private Label audioStatusLabel;
+
+    @FXML
+    private Button muteButton;
 
     // Method to set the previous scene
     public void setPreviousScene(Scene previousScene) {
@@ -26,8 +45,69 @@ public class SoundController {
         }
     }
 
-    public void onMuteButtonClick() {
+    // Initialize the controller after FXML file is loaded
+    @FXML
+    private void initialize() {
+        // Set up a listener for the volume slider
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            // Update the volume text field when the slider is moved
+            volumeTextField.setText(String.valueOf(newValue.intValue()));
+        });
 
+        // Add a listener for the volume text field focus property
+        volumeTextField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                // When focus is lost, update the slider with the typed volume
+                updateVolumeFromTextField();
+            }
+        });
+
+        // Set default values
+        volumeSlider.setValue(previousVolume);
+        volumeTextField.setText(String.valueOf(previousVolume));
+    }
+
+    // Method to handle volume change from the text field
+    public void onVolumeTextFieldChanged(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            updateVolumeFromTextField();
+        }
+    }
+
+    private void updateVolumeFromTextField() {
+        try {
+            // Parse the text field value to an integer
+            int volume = Integer.parseInt(volumeTextField.getText());
+
+            // Ensure the volume is within the valid range (0-100)
+            volume = Math.max(0, Math.min(100, volume));
+
+            // Update the slider value
+            volumeSlider.setValue(volume);
+        } catch (NumberFormatException e) {
+            // Handle the case where the input is not a valid integer
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void onMuteButtonClick() {
+        if (volumeSlider.getValue() > 0) {
+            // Store the current volume in a variable
+            previousVolume = (int) volumeSlider.getValue();
+
+            // Mute the audio by setting the volume to zero
+            volumeSlider.setValue(0);
+
+            // Update the button text to indicate the mute state
+            muteButton.setText("Audio: Muted");
+        } else {
+            // Unmute the audio by setting the volume back to the previous state
+            volumeSlider.setValue(previousVolume);
+
+            // Update the button text to indicate the unmute state
+            muteButton.setText("Audio: Unmuted");
+        }
     }
 
     // Event handler for the "Return" button
