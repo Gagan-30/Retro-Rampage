f37a2a46dee380891b201e9e071a91ebb52e4972
MalwareDetectedBlog <MalwareDetected0@gmail.com>
2024-02-03 20:27:36 +0000

    Filled with 1x1 grid. Need to add multithreading as performance is slow

--- src/main/java/com/base/game/retrorampage/LevelGeneration/Main.java
+++ src/main/java/com/base/game/retrorampage/LevelGeneration/Main.java
@@ -20,15 +20,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        // Prepare input data
-        Polygon polygon = new Polygon(Arrays.asList(new PolygonPoint(0, 0, 0),
-                new PolygonPoint(10, 0, 1), new PolygonPoint(10, 10, 2), new PolygonPoint(0, 10, 3)));
-        // Launch tessellation
-        Poly2Tri.triangulate(polygon);
-        // Gather triangles
-        List<DelaunayTriangle> triangles = polygon.getTriangles();
-        System.out.println(triangles);
-
         launch(args);
     }
 }
